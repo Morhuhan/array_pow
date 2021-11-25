5a5235d6766e05b7a0bec65ae9744ce315fd8825
morhuhan <radionovich.arkadiy@mail.ru>
2021-11-25 22:28:07 +0500

    finally commit

--- array_pow.c
+++ array_pow.c
@@ -1,20 +1,9 @@
 #include <stdio.h>
 #include <math.h>
 
-void main(void) {
-    int flag = 0;
-    printf ("Please, enter the array \n");
-    printf ("Your array is: 4, 4, 4, -4, -5, -6, 7, -8, 9, 10 \n");
-    int A[10] = {4, 4, 4, -4, -5, -6, 7, -8, 9, 10};
-
-    for (int i = 0; i < 10; i++) {
-        if (A[i] <= -7) {
-            flag = 1;
-        }
-    }
-
+void complate(int* A, int flag) {
     if (flag != 0) {
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 10; i++) {       //если есть то производим некие операции
             if (A[i] < 0) {
                 A[i] = A[i] * A[i];
             }
@@ -23,15 +12,32 @@ void main(void) {
             }
         }
     }
-    else {
+    else {                                   //если нет то просто умножаем все на 2
         for (int i = 0; i < 10; i++) {
             A[i] = A[i] * 2;
         }
     }
+}
 
-    printf ("Finally array is: ");
+int check(int* A) {
+    int flag;
+    for (int i = 0; i < 10; i++) {           //проверяем есть ли в массиве числа <= -7
+        if (A[i] <= -7) {
+            flag = 1;
+        }
+    }
+    return flag;
+}
 
+void main(void) {
+    int flag = 0;
+    printf("Please, enter the array \n");
+    printf("Your array is: 4, 4, 4, -4, -5, -6, 7, -8, 9, 10 \n");
+    int A[10] = {4, 4, 4, -4, -5, -6, 7, -8, 9, 10};  //задаем массив
+    flag = check(A);
+    complate(A, flag);
+    printf("Finally array is: ");             //выводим обработаный массив
     for (int i = 0; i < 10; i++) {
-        printf ("%d ", A[i]);
+        printf("%d ", A[i]);
     }
 }
